c92566bb8797c6421a14baed2ea84d8e090884d4
Kishan Jadav <kishan_jadav@hotmail.com>
2024-08-17 20:50:43 +0100

    Initial Commit

---
+++ sudoku Watch App/sudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sudokuApp.swift
+//  sudoku Watch App
+//
+//  Created by Kishan Jadav on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct sudoku_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
